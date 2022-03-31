0aac6363b7ec9123c6375f926996a0a9b9e62a0c
Lauren Black <laurenblack5280@gmail.com>
2022-03-30 20:25:04 -0600

    AddUnicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,22 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
   }
+
+isWhite() {
+  if(this.color === "white") {
+    return true
+  }
+  return false
+}
+says(message) {
+
+  return  `**;* ${message} *;**` 
+}
+
+
+
 }
 
 module.exports = Unicorn;
