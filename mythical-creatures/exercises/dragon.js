9784b77aea3e4eb45f54ebcf61e6cc3265590448
Lauren Black <laurenblack5280@gmail.com>
2022-04-01 16:06:19 -0600

    Add Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,43 @@
+class Dragon {
+  constructor(name, rider) {
+      this.name = name;
+      this.rider = rider;
+      this.hungry = true;
+      this.stomach = 0
+  }
 
+greet() {
+  return "Hi, " + this.rider + "!";
+}
+
+eat() {
+  this.stomach ++;
+  if (this.stomach === 3) {
+    this.hungry = false;
+  };
+}
+}
+
+
+
+
+
+// eat() {
+//   if(this.hungry === hungry) {
+//     return true
+//     }
+//     return false
+//   }
+//
+// }
+
+
+
+
+
+
+
+
+
+
+module.exports = Dragon;
